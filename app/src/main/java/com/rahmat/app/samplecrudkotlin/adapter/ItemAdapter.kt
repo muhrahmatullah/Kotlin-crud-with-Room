9e1a2245db7c2c80697acf1c28261baf2c139c68
Muhammad Rahmatullah <muhammadrahmatullah1@gmail.com>
2018-08-12 12:53:18 +0700

    add adapter

---
+++ app/src/main/java/com/rahmat/app/samplecrudkotlin/adapter/ItemAdapter.kt
@@ -0,0 +1,7 @@
+package com.rahmat.app.samplecrudkotlin.adapter
+
+/**
+ * Created by muhrahmatullah on 12/08/18.
+ */
+class ItemAdapter {
+}
---
+++ app/src/main/java/com/rahmat/app/samplecrudkotlin/entity/Student.kt
@@ -0,0 +1,7 @@
+package com.rahmat.app.samplecrudkotlin.entity
+
+/**
+ * Created by muhrahmatullah on 12/08/18.
+ */
+class Student {
+}
