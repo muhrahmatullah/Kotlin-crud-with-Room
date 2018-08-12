e686f899231f8f0a8908302b721cd569494fd137
Muhammad Rahmatullah <muhammadrahmatullah1@gmail.com>
2018-08-12 18:38:11 +0700

    add detail activity

---
+++ app/src/main/java/com/rahmat/app/samplecrudkotlin/DetailActivity.kt
@@ -0,0 +1,23 @@
+package com.rahmat.app.samplecrudkotlin
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_detail.*
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+}
